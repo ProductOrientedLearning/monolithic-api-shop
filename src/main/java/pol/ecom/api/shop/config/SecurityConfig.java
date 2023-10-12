48bbba63cd76c2032fdae25ff829a91f2815ca85
Nguyen Manh Ha <manhhad32@gmail.com>
2023-10-12 20:14:38 +0700

    update version sping boot to 3.1.4

--- src/main/java/pol/ecom/api/shop/config/SecurityConfig.java
+++ src/main/java/pol/ecom/api/shop/config/SecurityConfig.java
@@ -24,6 +24,7 @@ package pol.ecom.api.shop.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -42,15 +43,17 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable().authorizeHttpRequests()
-                .requestMatchers("/swagger-resources/**").permitAll()
-                .requestMatchers("/v3/api-docs/**").permitAll()
-                .requestMatchers("/swagger-ui/**").permitAll()
-                .requestMatchers("/**").authenticated().and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+        httpSecurity.authorizeHttpRequests( auth ->{ auth
+                        .requestMatchers(HttpMethod.GET, "/*").permitAll()
+                        .requestMatchers("/swagger-resources/**").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
+                        .requestMatchers("/swagger-ui/**").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/**").authenticated();
+                })
                 .addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class);
+        ;
         return httpSecurity.build();
     }
+
 }
