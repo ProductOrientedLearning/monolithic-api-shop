cd93e2d066b539875c572d68f78e2790a4c9e699
Nguyen Manh Ha <manhhad32@gmail.com>
2023-10-13 14:51:57 +0700

    update for security apis

--- src/main/java/pol/ecom/api/shop/config/SecurityConfig.java
+++ src/main/java/pol/ecom/api/shop/config/SecurityConfig.java
@@ -27,6 +27,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -43,7 +44,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests( auth ->{ auth
+        httpSecurity.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth ->{ auth
                         .requestMatchers(HttpMethod.GET, "/*").permitAll()
                         .requestMatchers("/swagger-resources/**").permitAll()
                         .requestMatchers("/v3/api-docs/**").permitAll()
